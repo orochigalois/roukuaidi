983c4ee2286a296744a008359939708dba8f61dc
Alex Yin <alex@jimmyweb.net>
2021-02-23 14:18:18 +1100

    update

--- wp-content/themes/roukuaidi/footer.php
+++ wp-content/themes/roukuaidi/footer.php
@@ -1,5 +1,13 @@
 	</main> <!-- end main -->
 	<footer class="footer">
+		<div class="container">
+			<div class="footer_wrap">
+				<p>Austagri development pty ltd. Sydney meat market. ©版权所有 <?php echo date("Y"); ?></p>
+				<a href="tel:0408880369">电话: 0408880369</a>
+				<a href="https://goo.gl/maps/9YNWZLVBMFa9mHVQ9">地址: 1 Madeline St, Strathfield South NSW 2136</a>
+			</div>
+		</div>
+
 
 	</footer><!-- #footer end -->
 	</div> <!-- end wrapper -->
