c739fa92f31977e5ef413dd0f6029816e8bfe5fd
Alex Yin <alex@jimmyweb.net>
2021-03-18 17:22:50 +1100

    update

--- wp-content/themes/roukuaidi/woocommerce/cart/cart.php
+++ wp-content/themes/roukuaidi/woocommerce/cart/cart.php
@@ -46,6 +46,8 @@ do_action('woocommerce_before_cart'); ?>
 
 					if ($_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters('woocommerce_cart_item_visible', true, $cart_item, $cart_item_key)) {
 						$product_permalink = apply_filters('woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink($cart_item) : '', $cart_item, $cart_item_key);
+
+						$product_permalink=NULL;
 				?>
 						<tr class="woocommerce-cart-form__cart-item <?php echo esc_attr(apply_filters('woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key)); ?>">
 
